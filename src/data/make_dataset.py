93ef82050565c9c2e79e15bdbdc4c91a361bdcb3
Rail <r.sayfeev@yandex.ru>
2023-11-05 19:50:24 +0300

    download and generate

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,18 @@
 import pandas as pd
 from datasets import Dataset, DatasetDict
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+
+
+def download_and_unzip(url, extract_to='.'):
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
+
+
+download_and_unzip("https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip",
+                   extract_to='../../data/raw')
 
 df = pd.read_table("../../data/raw/filtered.tsv")
 
